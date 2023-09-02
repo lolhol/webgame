56db08a1c638a9e8c35927db3cc2a0a96742f2b9
godbrigero <golden.notch.creeper@gmail.com>
2023-09-01 23:11:05 -0700

    remove stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,5 @@ import Image from "next/image";
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    const handleKeyPress = (e: any) => {
-      if (e.key === "s" || e.key === "S") {
-        console.log("The A key was pressed!123");
-        count++;
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyPress);
-  }, []);
-
   return <div className="Home"></div>;
 }
